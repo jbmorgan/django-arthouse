380b83fd26d30fe1a5052a62f05d40f7bb194fb9
Jonathan Morgan <jbmorgan@ncsu.edu>
2013-01-02 12:45:49 -0500

    Added basic django admin panel functionality.

---
+++ arthouse/admin.py
@@ -0,0 +1,8 @@
+from arthouse.models import *
+from django.contrib import admin
+
+admin.site.register(Person)
+admin.site.register(Movie)
+admin.site.register(TicketType)
+admin.site.register(PriceSheet)
+admin.site.register(Showtime)
